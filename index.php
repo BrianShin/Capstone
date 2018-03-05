c4689043c83e06cd389d02e1ad6f7c117ce264cb
Brian Shin <brian.sooyong.shin@gmail.com>
2018-03-05 12:01:12 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>K12toCollege</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet"  href="css/bootstrap.min.css">
+		<link rel="stylesheet"  href="styles.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	</head>
+	<body >
+			<?php header('Location: View/login/login.php');?>
+  </body>
+  </html>
